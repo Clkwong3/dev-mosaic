900b44665ffd52e46bc250f51ceadc4999a8c64b
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-04 00:57:55 -0700

    Wrap nav in a header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,21 @@
 const Navigation = ({ changePage }) => {
   // Create a navigation bar with links that trigger the changePage function
   return (
-    <nav className="navbar-menu ">
-      <div className="link" onClick={() => changePage("aboutMe")}>
-        <a href="/about">About Me</a>
-      </div>
-      |
-      <div className="link" onClick={() => changePage("portfolio")}>
-        <a href="/portfolio">Portfolio</a>
-      </div>
-      |
-      <div className="link" onClick={() => changePage("contactMe")}>
-        <a href="/contact">Contact Me</a>
-      </div>
-    </nav>
+    <header>
+      <nav className="navbar-menu ">
+        <div className="link" onClick={() => changePage("aboutMe")}>
+          <a href="/about">About Me</a>
+        </div>
+        |
+        <div className="link" onClick={() => changePage("portfolio")}>
+          <a href="/portfolio">Portfolio</a>
+        </div>
+        |
+        <div className="link" onClick={() => changePage("contactMe")}>
+          <a href="/contact">Contact Me</a>
+        </div>
+      </nav>
+    </header>
   );
 };
 
