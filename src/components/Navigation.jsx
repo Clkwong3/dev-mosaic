c0ee3bdbba5b13715ae22d3604080b09b86c46e0
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-04 00:02:51 -0700

    Stylize Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,18 @@
 const Navigation = ({ changePage }) => {
-  // Define styles for links and separators
-  const linkStyle = { margin: "10px" };
-  const separatorStyle = { margin: "5px" };
-
   // Create a navigation bar with links that trigger the changePage function
   return (
-    <nav className="navbar-menu">
-      <section>
-        <div style={linkStyle} onClick={() => changePage("aboutMe")}>
-          <a href="#">About Me</a>
-        </div>
-        <span style={separatorStyle}>|</span>{" "}
-        <div style={linkStyle} onClick={() => changePage("portfolio")}>
-          <a href="#">Portfolio</a>
-        </div>
-        <span style={separatorStyle}>|</span>
-        <div style={linkStyle} onClick={() => changePage("contactMe")}>
-          <a href="#">Contact Me</a>
-        </div>
-      </section>
+    <nav className="navbar-menu ">
+      <div className="link" onClick={() => changePage("aboutMe")}>
+        <a href="/about">About Me</a>
+      </div>
+      |
+      <div className="link" onClick={() => changePage("portfolio")}>
+        <a href="/portfolio">Portfolio</a>
+      </div>
+      |
+      <div className="link" onClick={() => changePage("contactMe")}>
+        <a href="/contact">Contact Me</a>
+      </div>
     </nav>
   );
 };
