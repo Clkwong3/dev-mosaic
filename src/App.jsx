cbdfbbd1e6a2cf629fbbcfdf64e03072c0a19d3d
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-04 01:20:12 -0700

    Ternary operators in the navigation.jsx and cleaned up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { useState } from "react";
-
-import Navigation from "./components/Navigation";
-import AboutMe from "./components/pages/AboutMe";
-import Portfolio from "./components/pages/Portfolio";
-import ContactMe from "./components/pages/ContactMe";
-
-import "./App.css";
 import { Outlet } from "react-router-dom";
 
+import Navigation from "./components/Navigation";
+
+import "./App.css";
+
 function App() {
-  // Use state to track the currently displayed page
-  const [currentPage, setCurrentPage] = useState("aboutMe");
-
-  // Change the currently displayed page
-  const changePage = (page) => {
-    setCurrentPage(page);
-  };
-
   // Render the Navigation component and the appropriate page based on the current selection
   return (
     <div>
-      <Navigation changePage={changePage} currentPage={currentPage} />
+      <Navigation />
       <Outlet />
     </div>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,27 @@
-const Navigation = ({ changePage }) => {
+const Navigation = ({ changePage, currentPage }) => {
   // Create a navigation bar with links that trigger the changePage function
+  console.log(currentPage);
   return (
     <header>
       <nav className="navbar-menu ">
-        <div className="link" onClick={() => changePage("aboutMe")}>
+        <div
+          className={currentPage === "aboutMe" ? "link active" : "link"}
+          onClick={() => changePage("aboutMe")}
+        >
           <a href="/about">About Me</a>
         </div>
         |
-        <div className="link" onClick={() => changePage("portfolio")}>
+        <div
+          className={currentPage === "portfolio" ? "link active" : "link"}
+          onClick={() => changePage("portfolio")}
+        >
           <a href="/portfolio">Portfolio</a>
         </div>
         |
-        <div className="link" onClick={() => changePage("contactMe")}>
+        <div
+          className={currentPage === "contactMe" ? "link active" : "link"}
+          onClick={() => changePage("contactMe")}
+        >
           <a href="/contact">Contact Me</a>
         </div>
       </nav>
