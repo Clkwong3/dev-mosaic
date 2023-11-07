fcc77e57bb3ce3eda43f802d08b30094658ea6c0
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-06 18:34:01 -0800

    Contact-Me Page - removed the contact-me section and import the ContactForm

--- src/components/pages/ContactMe.jsx
+++ src/components/pages/ContactMe.jsx
@@ -5,26 +5,12 @@ import ContactForm from "../ContactForm";
 const ContactMe = () => {
   return (
     // Create a container div with the class "contact-content"
-    <div className="contact-content">
-      <h1>Contact Me</h1> 
-      <p>Feel free to reach out to me. I look forward to hearing from you!</p>{" "}
+    <div className="contact-content face">
+      <h1>Contact Me</h1>
+      <p>
+        Feel free to reach out to me. I look forward to hearing from you!
+      </p>
       <ContactForm />
-      <div className="contact-info">
-        <p>You can also connect with me through:</p>{" "}
-        <ul>
-          <li>
-            <a href="mailto:your.email@example.com">
-              Email: your.email@example.com
-            </a>
-          </li>
-          <li>
-            <a href="[Your LinkedIn URL]">LinkedIn: [Link]</a>
-          </li>
-          <li>
-            <a href="https://github.com/Clkwong3">GitHub</a>
-          </li>
-        </ul>
-      </div>
     </div>
   );
 };
